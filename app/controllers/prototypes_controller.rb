f7ff9d3148b17c4e04778004052c23c6dc2683f0
sumiremama <yumi.kikuchi.0621@gmail.com>
2023-10-27 13:05:12 +0900

    prototypesコントローラーにbefore_actionを追加しました

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  #before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   before_action :set_prototype, except: [:index, :new, :create]
 
   def index
@@ -48,9 +48,9 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  #def contributor_confirmation
-    #redirect_to root_path unless current_user == @prototype.user
-  #end
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
+  end
 
   def set_prototype
     @prototype = Prototype.find(params[:id])
