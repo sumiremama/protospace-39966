dbb4bda08f65f0f56a38dffa2ffd7feb175e9b2f
sumiremama <yumi.kikuchi.0621@gmail.com>
2023-10-27 16:42:29 +0900

    コントローラーにおけるbeforeアクションの並べ替え

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   before_action :set_prototype, except: [:index, :new, :create]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -48,11 +48,11 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def contributor_confirmation
-    redirect_to root_path unless current_user == @prototype.user
-  end
-
   def set_prototype
     @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
